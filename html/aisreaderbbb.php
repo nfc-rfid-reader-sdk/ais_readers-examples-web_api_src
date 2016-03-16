7348d059739cb67978cc1e34ec45d03fca043919
developer3 <support@d-logic.rs>
2016-03-16 07:27:14 +0000

    removed deprecated php files
    git-svn-id: svn://oldsvnserver/sw_open/ais_readers/python/examples/http/trunk@28 0228885a-6ad7-41a9-bcf9-d0ec016d0fc8

--- html/aisreaderbbb.php
+++
@@ -1,9 +0,0 @@
-//$postprovera = file_get_contents("php://input")
-
-<?php
-    $postprovera = file_get_contents("php://input") 
-	$choise = $_POST['choise'];
-	echo $choise;
-	echo $postprovera;
-
-?>
