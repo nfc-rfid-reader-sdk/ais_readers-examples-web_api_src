fd06e49b1bde338c8fd123890211273b9024ad6b
developer8 <support@d-logic.rs>
2016-04-13 10:14:39 +0000

    ver 2.0.2.0
    - resolved output for more functions in 'ais_shell.py' script
    
    git-svn-id: svn://oldsvnserver/sw_open/ais_readers/python/examples/web_api/trunk@37 0228885a-6ad7-41a9-bcf9-d0ec016d0fc8

--- ais_http.py
+++ ais_http.py
@@ -2,7 +2,8 @@
 
 """
 @author: Vladan S
-@version: 2.0.1.8 (build)  (lib:4.9.4)
+@version: 2.0.2.0   (lib:4.9.6)
+@copyright: D-Logic   http://www.d-logic.net/nfc-rfid-reader-sdk/
  
 """
 
@@ -29,20 +30,35 @@ from constants import *
 
 def http_request(path, post_attrib):
     try:
-        req = urllib2.Request(path, post_attrib)
+        req = urllib2.Request(path, post_attrib)        
         req.add_header("Content-type", "application/x-www-form-urlencoded")
         page = urllib2.urlopen(req).read()
+        return page
     except Exception as e:
-        print e
+        return e
+
 
 class GetHandler(BaseHTTPRequestHandler):
- 
+   
+    global url_query
+    
     def do_GET(self):
         pass
-        return
-    
+        try: 
+           
+            f = open(os.curdir+os.sep+"ais_readers.html")
+            self.send_response(200)
+            self.send_header("Content-type","text/html")
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()            
+            return
+        except IOError:
+            self.send_error(404,"File Not Found: %s" % self.path)
+        return    
     
     def do_POST(self):
+        global url_query
         pass                
         dev              = DEV_HND       
         ctype, pdict = cgi.parse_header(self.headers['content-type'])
@@ -54,204 +70,237 @@ class GetHandler(BaseHTTPRequestHandler):
         else:
             pq = {}
       
-        try:  
+      
+        #try:  
            
-            f             = ''.join(pq[FUNCTION])             
-            seconds       = int(''.join(pq[RTE]))
-            device        = ''.join(pq[DEVICE])
-                     
-            if device == None or device == '':
-                dev.hnd  = HND_LIST[0]
-                device   = dev.hnd          
-            else:
-                dev.hnd  = HND_LIST[int(device) - 1]
-          
-          
-           
-            if pq[START_INDEX] != None or pq[END_INDEX] != None:
-               start_index = (''.join(pq[START_INDEX]))
-               end_index   = (''.join(pq[END_INDEX]))
-              
-            
-            if pq[START_TIME] != None or pq[END_TIME] != None:
-               start_time = (''.join(pq[START_TIME]))        
-               end_time   = (''.join(pq[END_TIME]))
-             
-               
-            if pq[WHITE_LIST_WRITE] != None:            
-                white_list_write = ''.join(pq[WHITE_LIST_WRITE])
-               
-            
-            if pq[BLACK_LIST_WRITE]!= None:
-                black_list_write = ''.join(pq[BLACK_LIST_WRITE])  
-            
-            if pq[DEFAULT_APP_PASS] != None:
-                set_def_pass = ''.join(pq[DEFAULT_APP_PASS])
-                
-            if pq[NEW_PASS] != None:
-                new_pass  = ''.join(pq[NEW_PASS])
-                       
-            if pq[UNREAD_LOG] != None:                 
-                get_unread_log = (''.join(pq[UNREAD_LOG]))
-            
-            if pq[LIGHTS] != None:
-                lights_choise = ''.join(pq[LIGHTS])   
-                               
-            if pq[EDIT_LIST] != None:
-                edit_list_choise = ''.join(pq[EDIT_LIST])
-               
-                      
-            if pq[DEVICE_TYPE] != None:
-                device_type = ''.join(pq[DEVICE_TYPE])
-            
-            if pq[DEVICE_ID] != None:
-                device_id  = ''.join(pq[DEVICE_ID])
-            
-           
-            
-            if f == 'q':                
-                self.wfile.write(GetListInformation())
-                
-            elif f == 'o':
-                #self.AISOpen() #self.wfile.write("<h1>" + str(list_res([0][0])) + "</h1>")              
-                pass                
-                self.wfile.write(AISOpen())                
-                    
-            elif f == 'c':
-                self.wfile.write(AISClose())
-            
-            if f == 'd':                             
-                self.wfile.write('GET DEVICES COUNT > %s\n' % AISUpdateAndGetCount())                        
+        f             = ''.join(pq[FUNCTION])             
+        seconds       = int(''.join(pq[RTE]))
+        device        = ''.join(pq[DEVICE])                    
         
-            elif f == 't': 
-                self.wfile.write(active_device()) 
-                self.wfile.write(AISGetTime())       
+      
+        if f == "D":               
+            self.send_response(200)
+            self.send_header("Content-type","text/html")
+            self.end_headers()
+            c =  open(os.curdir+os.sep+"bbb_logs.log")
+            self.wfile.write("<html><head><title>Read Debug Log</title></head><body>")
+            for line in c:                    
+                l = line.encode("utf-8") 
+                self.wfile.write("<p>%s</p>" % l)                                   
+            c.close()
+            self.wfile.write("</body></html>")
+            self.wfile.close()
+            return
+        
+        
+        if f == "S":
+            c =  open(os.curdir+os.sep+"bbb_logs.log")
+            self.send_response(200)
+            self.send_header("Content-type","text/json")
+            self.end_headers()
+            self.wfile.write(c.read())
+            c.close() 
+            self.wfile.close()                
+            return
+
+
+        
+        
+        if device == None or device == '':
+            dev.hnd  = HND_LIST[0]
+            device   = dev.hnd          
+        else:
+            dev.hnd  = HND_LIST[int(device) - 1]
+                          
+        if pq[START_INDEX] != None or pq[END_INDEX] != None:
+           start_index = (''.join(pq[START_INDEX]))
+           end_index   = (''.join(pq[END_INDEX]))
+          
+        
+        if pq[START_TIME] != None or pq[END_TIME] != None:
+           start_time = (''.join(pq[START_TIME]))        
+           end_time   = (''.join(pq[END_TIME]))
+         
+           
+        if pq[WHITE_LIST_WRITE] != None:            
+            white_list_write = ''.join(pq[WHITE_LIST_WRITE])
+           
+        
+        if pq[BLACK_LIST_WRITE]!= None:
+            black_list_write = ''.join(pq[BLACK_LIST_WRITE])  
+        
+        if pq[DEFAULT_APP_PASS] != None:
+            set_def_pass = ''.join(pq[DEFAULT_APP_PASS])
             
-            elif f == 'T':             
-                self.wfile.write(sys_get_timezone_info()+ "\n")
-                self.wfile.write(AISSetTime())
-            
-            elif f == 'E':
-                pass                
-                stop_time = c_uint64()
-                stop_time = time.time() + seconds #10
-                dev       = DEV_HND
-                self.wfile.write("Wait for RTE for %d sec ...\n" % seconds)       
-                while (time.ctime(time.time()) < time.ctime(stop_time)) :
-                    for hnd in HND_LIST:
-                        dev.hnd = hnd            
-                        r,rte = MainLoop()                       
-                        self.wfile.write(rte)
-                    time.sleep(THD_SLEEP)     
-                self.wfile.write ("End RTE listen")    
-                
+        if pq[NEW_PASS] != None:
+            new_pass  = ''.join(pq[NEW_PASS])
+                   
+        if pq[UNREAD_LOG] != None:                 
+            get_unread_log = (''.join(pq[UNREAD_LOG]))
+        
+        if pq[LIGHTS] != None:
+            lights_choise = ''.join(pq[LIGHTS])   
+                           
+        if pq[EDIT_LIST] != None:
+            edit_list_choise = ''.join(pq[EDIT_LIST])
+           
                   
-            elif f == 'l':                
-                self.wfile.write(log_get()) 
-           
-            elif f == 'n':                                             
-                self.wfile.write(log_by_index(int(start_index),int(end_index)))               
+        if pq[DEVICE_TYPE] != None:
+            device_type = ''.join(pq[DEVICE_TYPE])
+        
+        if pq[DEVICE_ID] != None:
+            device_id  = ''.join(pq[DEVICE_ID])
+        
+       
+       
+        
             
-            elif f == 'N':                                          
-                self.wfile.write(log_by_time(int(start_time),int(end_time)))  
-
-            elif f == 'u':                
-                get_unread_log = int(get_unread_log)                                
-                self.wfile.write(get_unread_log_one(get_unread_log))
-           
-            elif f == 'v':            
-                self.wfile.write("AIS_GetDLLVersion() >> %s\n" % AISGetLibraryVersionStr())
-                      
-            elif f == 'w':               
-                self.wfile.write(whitelist_read())
-                
-            elif f == 'b':                
-                self.wfile.write(blacklist_read())
-
-            elif f == 'W':                                    
-                self.wfile.write(whitelist_write(white_list_write))                                  
-
-            elif f == 'B':                                                
-                self.wfile.write(blacklist_write(black_list_write))                                 
-
-            elif f == 'L':                          
-                self.wfile.write(TestLights(lights_choise))
-                
-            elif f == 'g':               
-                self.wfile.write(get_io_state())
-            elif f == 'G':               
-                self.wfile.write(lock_open())
-            elif f == 'y':               
-                self.wfile.write(relay_toogle())
-          
-            elif f == 'P':                               
-                self.wfile.write("Actual application password is :%s\n" % PASS)
-                if  len(set_def_pass) == 0:
-                    self.wfile.write("Patch - new pass = default pass\n")
-                    set_def_pass = PASS
-                PASS = set_def_pass                    
-                self.wfile.write(password_set_default(set_def_pass)) 
-                
-            elif f == 'p':
-                self.wfile.write("Old password is actual application password: %s\n" % PASS)           
-                self.wfile.write("New password for units ( and application ): %s\n" % new_pass)
-                if  len(new_pass) == 0:
-                    self.wfile.write ("Patch - new pass = default pass\n")
-                    new_pass = PASS 
-                self.wfile.write("Try set new password for units= %s\n" % (new_pass))
-                self.wfile.write(password_change(new_pass))                       
-
-            elif f == 'd':
-                self.wfile.write(AISGetDevicesForCheck()) 
-          
-            elif f == 'f':               
-                self.wfile.write(AISGetVersion())
+       
+        
+        if f == 'q':                
+            self.wfile.write(GetListInformation())
             
-            elif f == 'i':              
-                self.wfile.write(AISGetVersion())
-                self.wfile.write(AISGetTime())
-                self.wfile.write(sys_get_timezone_info()+ "\n")
-           
-            elif f == 'Q':             
-                if edit_list_choise == AVAILABLE_DEVICES :            
-                    self.wfile.write(edit_device_list(1))
-                elif edit_list_choise == ACTUAL_LIST:
-                    self.wfile.write(edit_device_list(2))
-                elif edit_list_choise == CLEAR_LIST:
-                    self.wfile.write(edit_device_list(3))
-                elif edit_list_choise == ADD_DEVICE:
-                    self.wfile.write("AIS_List_AddDevicesForCheck() ...\n")
-                    self.wfile.write(edit_device_list(4,"AIS_List_AddDeviceForCheck",int(device_type),int(device_id)))
-                elif edit_list_choise == ERASE_DEVICE:
-                    self.wfile.write("AIS_List_EraseDeviceForCheck()...\n")
-                    self.wfile.write(edit_device_list(5,"AIS_List_EraseDeviceForCheck",int(device_type),int(device_id)))
-             
-                else:
-                    self.wfile.write("")
-           
-            elif f == 'x':
-                self.wfile.write("\nServer stopped !\nClose program !\n")            
-                shut_event.set()
-                httpd.server_close()                
-                if sys.platform.startswith('linux'):
-                    os.system('pkill -9 python')
-                elif sys.platform.startswith('win'):                    
-                    sys.exit(0)
+        elif f == 'o':
+            #self.AISOpen() #self.wfile.write("<h1>" + str(list_res([0][0])) + "</h1>")              
+            pass                
+            self.wfile.write(AISOpen())                
                 
-        except (Exception) as error_mess:
-                self.wfile.write(error_mess)
-                self.wfile.write(traceback.print_exc())
+        elif f == 'c':
+            self.wfile.write(AISClose())
+        
+        if f == 'd':                             
+            self.wfile.write('GET DEVICES COUNT > %s\n' % AISUpdateAndGetCount())                        
+    
+        elif f == 't': 
+            self.wfile.write(active_device()) 
+            self.wfile.write(AISGetTime())       
+        
+        elif f == 'T':             
+            self.wfile.write(sys_get_timezone_info()+ "\n")
+            self.wfile.write(AISSetTime())
+        
+        elif f == 'E':
+            pass                
+            stop_time = c_uint64()
+            stop_time = time.time() + seconds #10
+            dev       = DEV_HND
+            self.wfile.write("Wait for RTE for %d sec ...\n" % seconds)       
+            while (time.ctime(time.time()) < time.ctime(stop_time)) :
+                for hnd in HND_LIST:
+                    dev.hnd = hnd            
+                    r,rte = MainLoop()                       
+                    self.wfile.write(rte)
+                time.sleep(THD_SLEEP)     
+            self.wfile.write ("End RTE listen")    
+            
+              
+        elif f == 'l':                         
+            self.wfile.write(log_get())
+            
+       
+        elif f == 'n':                                                          
+            self.wfile.write(log_by_index(int(start_index),int(end_index)))               
+           
+            
+        elif f == 'N':                                                       
+            self.wfile.write(log_by_time(int(start_time),int(end_time)))  
+            
+            
+        elif f == 'u':                
+            get_unread_log = int(get_unread_log)                                
+            self.wfile.write(get_unread_log_one(get_unread_log))
+       
+        elif f == 'v':            
+            self.wfile.write("AIS_GetDLLVersion() >> %s\n" % AISGetLibraryVersionStr())
+                  
+        elif f == 'w':               
+            self.wfile.write(whitelist_read())
+            
+        elif f == 'b':                
+            self.wfile.write(blacklist_read())
+
+        elif f == 'W':                                    
+            self.wfile.write(whitelist_write(white_list_write))                                  
+
+        elif f == 'B':                                                
+            self.wfile.write(blacklist_write(black_list_write))                                 
+
+        elif f == 'L':                          
+            self.wfile.write(TestLights(lights_choise))
+            
+        elif f == 'g':               
+            self.wfile.write(get_io_state())
+        elif f == 'G':               
+            self.wfile.write(lock_open())
+        elif f == 'y':               
+            self.wfile.write(relay_toogle())
+      
+        elif f == 'P':                               
+            self.wfile.write("Actual application password is :%s\n" % PASS)
+            if  len(set_def_pass) == 0:
+                self.wfile.write("Patch - new pass = default pass\n")
+                set_def_pass = PASS
+            PASS = set_def_pass                    
+            self.wfile.write(password_set_default(set_def_pass)) 
+            
+        elif f == 'p':
+            self.wfile.write("Old password is actual application password: %s\n" % PASS)           
+            self.wfile.write("New password for units ( and application ): %s\n" % new_pass)
+            if  len(new_pass) == 0:
+                self.wfile.write ("Patch - new pass = default pass\n")
+                new_pass = PASS 
+            self.wfile.write("Try set new password for units= %s\n" % (new_pass))
+            self.wfile.write(password_change(new_pass))                       
+
+        elif f == 'd':
+            self.wfile.write(AISGetDevicesForCheck()) 
+      
+        elif f == 'f':               
+            self.wfile.write(AISGetVersion())
+        
+        elif f == 'i':              
+            self.wfile.write(AISGetVersion())
+            self.wfile.write(AISGetTime())
+            self.wfile.write(sys_get_timezone_info()+ "\n")
+       
+        elif f == 'Q':             
+            if edit_list_choise == AVAILABLE_DEVICES :            
+                self.wfile.write(edit_device_list(1))
+            elif edit_list_choise == ACTUAL_LIST:
+                self.wfile.write(edit_device_list(2))
+            elif edit_list_choise == CLEAR_LIST:
+                self.wfile.write(edit_device_list(3))
+            elif edit_list_choise == ADD_DEVICE:
+                self.wfile.write("AIS_List_AddDevicesForCheck() ...\n")
+                self.wfile.write(edit_device_list(4,"AIS_List_AddDeviceForCheck",int(device_type),int(device_id)))
+            elif edit_list_choise == ERASE_DEVICE:
+                self.wfile.write("AIS_List_EraseDeviceForCheck()...\n")
+                self.wfile.write(edit_device_list(5,"AIS_List_EraseDeviceForCheck",int(device_type),int(device_id)))
+         
+            else:
+                self.wfile.write("")
+       
+        elif f == 'x':
+            self.wfile.write("\nServer stopped !\nClose program !\n")            
+            shut_event.set()
+            httpd.server_close()                
+            if sys.platform.startswith('linux'):
+                os.system('pkill -9 python')
+            elif sys.platform.startswith('win'):                    
+                sys.exit(0)
+            
+    # except (Exception) as error_mess:
+            # self.wfile.write(error_mess)
+            # self.wfile.write(traceback.print_exc())
         return
         
 def RunAll():   
     serv = threading.Thread(target=handler_server)   
     serv.start()       
     while True:
-        try:          
-        
+        try:                  
             if serv.isAlive():            
-                serv.join(timeout = SERV_JOIN)                     
-        
+                serv.join(timeout = SERV_JOIN)                             
         except (KeyboardInterrupt,SystemExit,Exception) as e:
             httpd.server_close()
             print '\nServer stopped\nProgram close',e
@@ -264,6 +313,7 @@ def RunAll():
             
 
 def handler_server():
+    global httpd
     while not shut_event.is_set():        
         my_lock.acquire()                                                         
         httpd.handle_request()
@@ -284,8 +334,7 @@ my_lock = threading.Lock()
 shut_event = threading.Event()
 
 if __name__ == '__main__': 
-    global httpd 
-    
+    global httpd     
     # global mySO
     # mySO = GetPlatformLib() 
     init()     
